54d0895d53afa204155386731d0e698077bbc880
DrahanTatiana <natanya1993@gmail.com>
2020-06-08 23:04:59 +0300

    True commit

--- lesson_02/my_name.py
+++ lesson_02/my_name.py
@@ -1,29 +1,29 @@
 #1
-print('*******', '*', '*', '*', '*', '*', '*', sep='\t' * 3)
+print('*******', '*', '*', '*', '*', '*', '*', sep='\t' * 3, end='\n')
 #2
 print('   *', '     *', '   *', end='\t', sep='\t' )
 print('    * *', '    *', sep='\t'*2, end='\t')
-print(' ',' *', '   *', '  *   *', sep='\t'*2 )
+print(' ',' *', '   *', '  *   *', sep='\t'*2, end='\n' )
 #3
 print('   *', '    *', '    *', end='\t', sep='\t' )
 print('    *   *', '*', sep='\t'*2, end='\t')
 print(' ','  *', '  *',  sep='\t'*2, end='\t'*3 )
-print('*', '*', sep='\t'*2 )
+print('*', '*', sep='\t'*2,end='\n')
 #4
 print('   *', '   ***********', end='\t', sep='\t' )
 print('    *     *', '*', sep='\t'*2, end='\t')
 print(' ', '*',  sep='\t'*3, end='\t' )
 print('*', sep='\t', end='\t'*2)
-print('   ***********', )
+print('   ***********', end='\n')
 #5
 print('   *', end='\t', sep='\t' )
 print('  *', '  *', end='\t', sep='\t'*3 )
 print('    *        *', '*', sep='\t', end='\t')
 print(' ', '  *',  sep='\t'*3, end='\t'*3 )
-print('  *', '  *', sep='\t'*3 )
+print('  *', '  *', sep='\t'*3, end='\n')
 #6
 print('   *', end='\t', sep='\t' )
 print(' *', '   *', end='\t', sep='\t'*3 )
 print('*', '*', sep='\t'*3, end='\t')
 print(' ', '  *',  sep='\t'*3, end='\t'*3 )
-print(' *', '   *', sep='\t'*3 )
+print(' *', '   *', sep='\t'*3,end='\n' )
