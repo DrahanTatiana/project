5f953c18469531ab8052e5a5e535c9aa452cdf4e
DrahanTatiana <natanya1993@gmail.com>
2020-06-08 19:52:44 +0300

    First commit

---
+++ lesson_02/my_name.py
@@ -0,0 +1,29 @@
+#1
+print('*******', '*', '*', '*', '*', '*', '*', sep='\t' * 3)
+#2
+print('   *', '     *', '   *', end='\t', sep='\t' )
+print('    * *', '    *', sep='\t'*2, end='\t')
+print(' ',' *', '   *', '  *   *', sep='\t'*2 )
+#3
+print('   *', '    *', '    *', end='\t', sep='\t' )
+print('    *   *', '*', sep='\t'*2, end='\t')
+print(' ','  *', '  *',  sep='\t'*2, end='\t'*3 )
+print('*', '*', sep='\t'*2 )
+#4
+print('   *', '   ***********', end='\t', sep='\t' )
+print('    *     *', '*', sep='\t'*2, end='\t')
+print(' ', '*',  sep='\t'*3, end='\t' )
+print('*', sep='\t', end='\t'*2)
+print('   ***********', )
+#5
+print('   *', end='\t', sep='\t' )
+print('  *', '  *', end='\t', sep='\t'*3 )
+print('    *        *', '*', sep='\t', end='\t')
+print(' ', '  *',  sep='\t'*3, end='\t'*3 )
+print('  *', '  *', sep='\t'*3 )
+#6
+print('   *', end='\t', sep='\t' )
+print(' *', '   *', end='\t', sep='\t'*3 )
+print('*', '*', sep='\t'*3, end='\t')
+print(' ', '  *',  sep='\t'*3, end='\t'*3 )
+print(' *', '   *', sep='\t'*3 )
